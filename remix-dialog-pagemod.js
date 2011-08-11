51ea82d306adfa4a35e49b3bb09c669ae2e5998e
Atul Varma <varmaa@gmail.com>
2011-08-11 15:34:27 -0400

    made remix dialog pagemod use hintmanager instead of custom stuff.

--- remix-dialog-pagemod.js
+++ remix-dialog-pagemod.js
@@ -1,36 +1,50 @@
 (function() {
   var preview = $("#preview").contents();
-  var hints = preview.find(".remix-dialog-hints").clone();
-  $(document.body).append(hints.show());
+  var filters = {
+    matches: function(selector) {
+      return function(ui, target) {
+        var parent = $(target).closest(".element");
+        if (parent) {
+          var linkedNode = parent.data("linked-node");
+          return $(linkedNode).is(selector);
+        }
+        return false;
+      };
+    },
+    isOnAttributeValue: function(attr) {
+      return function(ui, target) {
+        return ($(target).is(".attributes .value") &&
+                $(target).prev(".name").text() == attr);      
+      };
+    },
+    isOnTextNode: function() {
+      return function(ui, target) {
+        return $(target).is(".element .text");
+      };
+    }
+  };
+  var hints = HintManager(null, filters);
 
-  function hintify(options) {
-    var filter = options.filter || function() { return true; };
-    $(options.part).live({
-      mouseover: function() {
-        var parent = $(this).closest(".element");
-        var linkedNode = parent.data("linked-node");
-        if (linkedNode == preview.find(options.target)[0] &&
-            filter.call(this))
-          options.hint.fadeIn();
-      },
-      mouseout: function() {
-        options.hint.fadeOut();
-      }
-    });
-  }
-
-  hintify({
-    target: "p.needs-fixing",
-    part: ".element .text",
-    hint: hints.find(".txt")
+  $(document.body).append(preview.find(".remix-dialog-hints").clone());
+  
+  hints.plant({
+    content: ".txt.hint",
+    when: {
+      matches: "p.needs-fixing",
+      isOnTextNode: true
+    }
   });
 
-  hintify({
-    target: "img#supergirl",
-    part: ".attributes .value",
-    filter: function() {
-      return $(this).prev(".name").text() == "src";
-    },
-    hint: hints.find(".attr")
+  hints.plant({
+    content: ".attr.hint",
+    when: {
+      matches: "img#supergirl",
+      isOnAttributeValue: "id"
+    }
+  });
+
+  $("#dom-rendering").live({
+    mouseover: function(event) { hints.refresh(event.target); },
+    mouseout: function(event) { hints.refresh(null); }
   });
 })();
